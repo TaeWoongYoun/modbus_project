f33971586352240e4937d3341020e77cabae7fa8
xodnd0507 <24457545yong@gmail.com>
2024-10-29 13:13:30 +0900

    style : Add table box

--- index.php
+++ index.php
@@ -8,31 +8,33 @@
 </head>
 <body>
     <h1>프로그램 데이터 조회</h1>
-    <table>
-        <thead>
-            <tr>
-                <th>번호</th>
-                <th>거리</th>
-                <th>습도</th>
-                <th>온도</th>
-                <th>날짜</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php
-                $sql = "SELECT idx, Distance, Humidity, Temperature, Date FROM program";
-                $result = mysqli_query($conn, $sql);
-                while ($row = mysqli_fetch_assoc($result)) {
-                    echo "<tr>
-                            <td>" . $row["idx"] . "</td>
-                            <td>" . $row["Distance"] . "</td>
-                            <td>" . $row["Humidity"] . "</td>
-                            <td>" . $row["Temperature"] . "</td>
-                            <td>" . $row["Date"] . "</td>
-                        </tr>";
-                    }
-            ?>
-        </tbody>
-    </table>
+    <div class="table-box">
+        <table>
+            <thead>
+                <tr>
+                    <th>번호</th>
+                    <th>거리</th>
+                    <th>습도</th>
+                    <th>온도</th>
+                    <th>날짜</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                    $sql = "SELECT * FROM program";
+                    $result = mysqli_query($conn, $sql);
+                    while ($row = mysqli_fetch_assoc($result)) {
+                        echo "<tr>
+                                <td>" . $row["idx"] . "</td>
+                                <td>" . $row["Distance"] . "</td>
+                                <td>" . $row["Humidity"] . "</td>
+                                <td>" . $row["Temperature"] . "</td>
+                                <td>" . $row["Date"] . "</td>
+                            </tr>";
+                        }
+                ?>
+            </tbody>
+        </table>
+    </div>
 </body>
 </html>
